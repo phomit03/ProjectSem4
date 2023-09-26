b728d2e0638779604a36155133460c99c6987e80
dominhtttb17 <dominhtttb17@gmail.com>
2023-09-26 20:28:54 +0700

    first commit

---
+++ eproject4/src/main/java/com/example/eproject4/Eproject4Application.java
@@ -0,0 +1,13 @@
+package com.example.eproject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eproject4Application.class, args);
+	}
+
+}
---
+++ eproject4/src/test/java/com/example/eproject4/Eproject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eproject4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eproject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
