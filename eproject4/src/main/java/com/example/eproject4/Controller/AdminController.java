fbf91d6162fe325c0d2dc3d8b2792ce9d7c7be07
nguyenvietanh <vietanh20030914@gmail.com>
2023-09-29 17:57:59 +0700

    admin index

---
+++ eproject4/src/main/java/com/example/eproject4/Controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.example.eproject4.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping("/admin")
+    public String admin() {
+        return "admin_index";
+    }
+}
