fee07d075474993e41a25a617a674b05835c5360
nguyenvietanh <vietanh20030914@gmail.com>
2023-10-09 19:18:16 +0700

    fix

--- eproject4/src/main/java/com/example/eproject4/Eproject4Application.java
+++ eproject4/src/main/java/com/example/eproject4/Eproject4Application.java
@@ -1,5 +1,7 @@
 package com.example.eproject4;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +10,30 @@ import org.springframework.context.annotation.ComponentScan;
 public class Eproject4Application {
 
 	public static void main(String[] args) {
+		setupSSHTunnel();
 		SpringApplication.run(Eproject4Application.class, args);
 	}
 
+	private static void setupSSHTunnel() {
+		String sshHost = "61.14.233.196"; // Địa chỉ máy chủ SSH
+		String sshUser = "hungqd"; // Tên đăng nhập SSH
+		String sshPassword = "RlKrZSrDH63YjZt"; // Mật khẩu SSH
+		int sshPort = 2018; // Cổng SSH
+
+		String dbHost = "localhost"; // Địa chỉ cơ sở dữ liệu MySQL thông qua SSH tunnel
+		int dbPort = 3306;
+
+		try {
+			JSch jsch = new JSch();
+			Session session = jsch.getSession(sshUser, sshHost, sshPort);
+			session.setPassword(sshPassword);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.connect();
+
+			int localPort = 3307;
+			session.setPortForwardingL(localPort, dbHost, dbPort);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
