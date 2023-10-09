c1c3bc573d6d50a4761cbacdf567c3021b541965
nguyenvietanh <vietanh20030914@gmail.com>
2023-10-09 22:13:21 +0700

    upload img cloudinary

--- eproject4/src/main/java/com/example/eproject4/Utils/Helper.java
+++ eproject4/src/main/java/com/example/eproject4/Utils/Helper.java
@@ -1,5 +1,8 @@
 package com.example.eproject4.Utils;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,26 +14,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class Helper {
-    private final String uploadImageDir = "./src/main/resources/static/img";
-
-    public Object[] uploadImage(MultipartFile file) throws IOException {
-        Object[] result = new Object[2];
-        result[0] = false;
-        try {
-            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-            Path uploadPath = Path.of(uploadImageDir);
-            Files.createDirectories(uploadPath);
-            Path filePath = uploadPath.resolve(fileName);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-            result[0] = true;
-            result[1] = "/img/" + fileName;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+    @Autowired
+    private Cloudinary cloudinary;
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        String imageUrl = (String) uploadResult.get("secure_url");
+        return imageUrl;
     }
 }
