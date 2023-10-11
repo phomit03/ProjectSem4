dc19a704c1df987f59405ba8d8c85ba1c5e7e747
lcisme <92872523+lcisme@users.noreply.github.com>
2023-10-11 22:52:45 +0700

    pageing

--- eproject4/src/main/java/com/example/eproject4/config/SecurityConfiguration.java
+++ eproject4/src/main/java/com/example/eproject4/config/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                        "/registration**",
+                        "/registration/**",
                         "/employee/**",
                         "/admin_static/**",
                         "/user/**",
