5664f444c26a6c14d1c24562772dd8223cdca921
dominhtttb17 <dominhtttb17@gmail.com>
2023-10-14 14:07:19 +0700

    innitial commit

--- eproject4/src/main/java/com/example/eproject4/DTO/UserRegistrationDto.java
+++ eproject4/src/main/java/com/example/eproject4/DTO/UserRegistrationDto.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.validation.constraints.Email;
-	import javax.validation.constraints.Size;
+
 
 import java.time.LocalDate;
 
