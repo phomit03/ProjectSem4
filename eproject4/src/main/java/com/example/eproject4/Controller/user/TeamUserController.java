67304e9cc6ec6050fe55bc0c5d55e119b323edec
lcisme <92872523+lcisme@users.noreply.github.com>
2023-10-26 12:14:35 +0700

    4 tran end 1 clb

--- eproject4/src/main/java/com/example/eproject4/Controller/user/TeamUserController.java
+++ eproject4/src/main/java/com/example/eproject4/Controller/user/TeamUserController.java
@@ -1,8 +1,7 @@
 package com.example.eproject4.Controller.user;
-
-import com.example.eproject4.DTO.Response.NewDTO;
-import com.example.eproject4.DTO.Response.PlayerDTO;
 import com.example.eproject4.DTO.Response.TeamDTO;
+import com.example.eproject4.Entity.Match;
+import com.example.eproject4.Service.MatchService;
 import com.example.eproject4.Service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +16,9 @@ import java.util.List;
 public class TeamUserController {
     @Autowired
     private TeamService teamService;
+    @Autowired
+
+    private MatchService matchService;
 
     @RequestMapping("")
     public String teams(Model model) {
@@ -31,12 +33,19 @@ public class TeamUserController {
     }
     @RequestMapping("/detail/{id}")
     public String teamDetail(@PathVariable Long id, Model model) {
-        model.addAttribute("title", "Teams Detail");
-        model.addAttribute("overlay_title", "Day la ten doi bong");
-        model.addAttribute("description", "Mo ta doi bong");
-
+        // Lấy thông tin của đội bóng
         TeamDTO teamDetail = teamService.getTeamById(id);
+        String tenDoiBong = teamDetail.getName();
+        String tenSVD = teamDetail.getHome_stadium();
+
+        // Lấy 4 trận đấu gần đây đã kết thúc của đội bóng dựa trên home_team_id hoặc away_team_id
+        List<Match> recentMatches = matchService.findRecentMatchesByTeamId(id);
+
+        model.addAttribute("title", "Teams Detail");
+        model.addAttribute("overlay_title", tenDoiBong);
+        model.addAttribute("description", tenSVD);
         model.addAttribute("teamDetail", teamDetail);
+        model.addAttribute("recentMatches", recentMatches);
 
         return "customer_team_detail";
     }
